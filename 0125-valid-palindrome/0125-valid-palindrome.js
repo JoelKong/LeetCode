c7460603e8787ed1ded08ae14c15026aebeaf0df
Joel Kong <joelkong2001@gmail.com>
2023-02-17 23:02:39 +0800

    Time: 85 ms (45.61%), Space: 44.2 MB (83.78%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi, "")
+    let left = 0, right = s.length - 1
+    
+    while (left < right) {
+        if (s[left] !== s[right]) return false
+        left++
+        right--
+    }
+    
+    return true
+};
+
+
+// convert to lower case and replace non-alpha with empty string
+// set 2 pointers, one at the start and one at the end
+// check both pointers if same letter then move the pointers towards each other
+// once left < right break out and return true else false
