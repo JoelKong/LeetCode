a2b549cb43d45670437a3ef5d9edc3f28f2165ea
Joel Kong <joelkong2001@gmail.com>
2023-02-18 20:11:59 +0800

    Time: 62 ms (69.83%), Space: 41.7 MB (67.74%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    let lastTwoSteps = [1, 1];
+    for (let i = 2; i <= n; i++) {
+        lastTwoSteps[i] = lastTwoSteps[i - 2] + lastTwoSteps[i - 1];
+    }
+    return lastTwoSteps[n]
+};
+
+// draw the actual staircase and analyse the ending of it
+// draw the tree
+// tabulation bottom up, if n is 1 theres only 1 way
+// set last 2 steps to have only 1 solution which is 1 way up
+// initialise at 2 since we already calculate 2 steps and loop till end of n
+// to calculate the next step is always the next step + the next next step
+// return lasttwosteps[n]
