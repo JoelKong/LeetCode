875185e447a5b4a4f513d9b42e5ceb67a7101bd1
Joel Kong <joelkong2001@gmail.com>
2023-02-18 11:50:21 +0800

    Time: 72 ms (55.23%), Space: 42.7 MB (42.30%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -34,3 +34,29 @@ var isValid = function(s, stack = []) {
 // else check whether the last element in the stack matches the value of the hashmap with the key representing the char
 // if exists, pop it off the stack, if not return false
 // if the stack is empty at the end return true else return false
+
+//
+
+/*
+var isValid = function(s) {   
+    const stack = [];
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s.charAt(i);
+        switch(c) {
+            case '(': stack.push(')');
+                break;
+            case '[': stack.push(']');
+                break;
+            case '{': stack.push('}');
+                break;
+            default:
+                if (c !== stack.pop()) {
+                    return false;
+                }
+        }
+    }
+    
+    return stack.length === 0;
+};
+*/
