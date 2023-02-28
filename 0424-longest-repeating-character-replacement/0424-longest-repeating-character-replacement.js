04f4dd22ddafeeae4bb1b330bd0f63c85444d153
Joel Kong <joelkong2001@gmail.com>
2023-02-28 22:38:04 +0800

    Time: 81 ms (81.35%), Space: 42.4 MB (77.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -23,3 +23,9 @@ var characterReplacement = function(s, k) {
     
     return res
 };
+
+// set up a hashmap count, res = 0, left pointer 0 and a maxFreq of 0
+// loop through the length of the string with r, map value of the char to be 1 + current value if not 0
+// calculate max freq with the current max and the value just added
+// while the length of the window(r-1+1) minus the maxfreq is more than k, remove the value (-1) of the map at the left pointer then increment the left pointer
+// after the while loop calculate the max of the result via the current max and length of the sliding window, return res
