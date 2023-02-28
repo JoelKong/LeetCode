5ba2597cc4ed0e3e43d161f63ddb38a78da7b0dd
Joel Kong <joelkong2001@gmail.com>
2023-02-28 21:37:29 +0800

    Time: 76 ms (82.69%), Space: 50 MB (13.96%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -2,59 +2,30 @@
  * @param {number[]} height
  * @return {number}
  */
-// var maxArea = function (height) {
-//     let max = 0
-//     let left = 0
-//     let right = height.length - 1
-    
-//     while (left < right) {
-//         const leftHeight = height[left]
-//         const rightHeight = height[right]
-//         const _height = Math.min(leftHeight, rightHeight)
-//         const width = right - left
-//         const area = _height * width
-//         max = Math.max(max, area)
-        
-//         if (leftHeight <= rightHeight) {
-//             left++
-//         }
-        
-//         if (rightHeight < leftHeight) {
-//             right++
-//         }
-//     }
-//     return max
-// }
-
-
 var maxArea = function (height) {
-    let [left, right, max] = [0, height.length - 1, 0];
-
+    let max = 0
+    let left = 0
+    let right = height.length - 1
+    
     while (left < right) {
-        const [leftHeight, rightHeight] = getHeights(height, left, right);
-        const area = getArea(height, left, right);
-
-        max = Math.max(max, area);
-
-        const isRightGreater = leftHeight <= rightHeight;
-        if (isRightGreater) left++;
-
-        const isRightLess = rightHeight < leftHeight;
-        if (isRightLess) right--;
+        const leftHeight = height[left]
+        const rightHeight = height[right]
+        const _height = Math.min(leftHeight, rightHeight)
+        const width = right - left
+        const area = _height * width
+        max = Math.max(max, area)
+        
+        if (leftHeight <= rightHeight) {
+            left++
+        }
+        
+        if (rightHeight < leftHeight) {
+            right--
+        }
     }
+    return max
+}
 
-    return max;
-};
-
-const getHeights = (height, left, right) => [height[left], height[right]];
-
-const getArea = (height, left, right) => {
-    const [leftHeight, rightHeight] = getHeights(height, left, right);
-    const _height = Math.min(leftHeight, rightHeight);
-    const width = right - left;
-
-    return _height * width;
-};
 
 // Init maxarea, left start 0, right start end
 // while left < right, init the left height and right height through indexing
