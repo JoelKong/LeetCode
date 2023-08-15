214bc44690239ff3f766cb996113b856e46fde0f
Joel Kong <joelkong2001@gmail.com>
2023-08-15 14:54:30 +0800

    Time: 81 ms (58.73%), Space: 19.5 MB (30.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,11 +5,10 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         hashmap = collections.defaultdict(list)
-        for string in strs:
-            key = [0] * 26
-            for char in string:
-                key[ord(char) - ord('a')] += 1
-            hashmap[tuple(key)].append(string)
-        return hashmap.values()
         
-    
+        for string in strs:
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            hashmap[tuple(count)].append(string)
+        return hashmap.values()
