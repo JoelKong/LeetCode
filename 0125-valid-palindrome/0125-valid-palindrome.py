413c33b0364ddbf1b3256e96a4217ac9551a5a90
Joel Kong <joelkong2001@gmail.com>
2023-08-21 13:34:23 +0800

    Time: 37 ms (56.65%), Space: 14.3 MB (70.53%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            while l < r and s[l].isalnum() is not True:
+                l += 1
+            while r > l and s[r].isalnum() is not True:
+                r -= 1          
+            
+            if (s[l].lower() != s[r].lower()):
+                return False
+            l += 1
+            r -= 1
+            
+        return True
+    
+    
+    
+    
+#     # Could write own alpha-numeric function
+#     def alphanum(self, c):
+#         return (
+#             ord("A") <= ord(c) <= ord("Z")
+#             or ord("a") <= ord(c) <= ord("z")
+#             or ord("0") <= ord(c) <= ord("9")
+#         )
