16e79c75c2de174a02a2897566b0c1aa6bfc32e2
Joel Kong <joelkong2001@gmail.com>
2023-09-02 16:44:50 +0800

    Time: 16 ms (47.35%), Space: 13.3 MB (50.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -14,12 +14,28 @@ class Solution(object):
         
 #         return n_solution
     
-        if n == 1:
-            return 1
-        lastTwoSteps = [1,1]
-        for i in range(2, n + 1):
-            nextStep = lastTwoSteps[i - 2] + lastTwoSteps[i - 1]
-            lastTwoSteps.append(nextStep)
+#         if n == 1:
+#             return 1
+#         lastTwoSteps = [1,1]
+#         for i in range(2, n + 1):
+#             nextStep = lastTwoSteps[i - 2] + lastTwoSteps[i - 1]
+#             lastTwoSteps.append(nextStep)
             
-        return lastTwoSteps[n]
+#         return lastTwoSteps[n]
         
+        
+        # if n is 0, we are already on that staircase so number of steps is 1. If n is 1, the number of steps is still 1.
+        # Beyond this, if n is 2, it is simply the sub problem of 1 and 0.
+        memo = [None] * (n + 1)
+        
+        def climbStairsHelper(n, memo):
+            if memo[n] != None:
+                return memo[n]
+            if n == 0 or n == 1:
+                result = 1
+            else:
+                result = climbStairsHelper(n - 1, memo) + climbStairsHelper(n - 2, memo)
+            memo[n] = result          
+            return result
+        
+        return climbStairsHelper(n, memo)
