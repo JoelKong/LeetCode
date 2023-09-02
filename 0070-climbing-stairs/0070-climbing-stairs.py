2482fc28189bfe632b4894bc8ddfd542830096c7
Joel Kong <joelkong2001@gmail.com>
2023-09-02 14:36:35 +0800

    Time: 12 ms (75.72%), Space: 13.2 MB (50.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,12 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        n_solution = 1
-        nprev_solution = 1
+#         n_solution = 1
+#         nprev_solution = 1
         
-        for i in range(n - 1):
-            temp = n_solution
-            n_solution = n_solution + nprev_solution
-            nprev_solution = temp
+#         for i in range(n - 1):
+#             temp = n_solution
+#             n_solution = n_solution + nprev_solution
+#             nprev_solution = temp
         
-        return n_solution
+#         return n_solution
+    
+        if n == 1:
+            return 1
+        lastTwoSteps = [1,1]
+        for i in range(2, n + 1):
+            nextStep = lastTwoSteps[i - 2] + lastTwoSteps[i - 1]
+            lastTwoSteps.append(nextStep)
+            
+        return lastTwoSteps[n]
+        
