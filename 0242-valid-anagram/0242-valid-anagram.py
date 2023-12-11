d7d239bbdab9628970f5d4c002572613c000bfa0
Joel Kong <joelkong2001@gmail.com>
2023-12-11 12:31:44 +0800

    Time: 33 ms (64.24%), Space: 15.2 MB (17.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,9 +7,11 @@ class Solution(object):
         """
         if len(s) != len(t):
             return False
+        s_count,t_count = {}, {}
         
-        countS, countT = {}, {}    
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+            s_count[s[i]] = 1 + s_count.get(s[i], 0)
+            t_count[t[i]] = 1 + t_count.get(t[i], 0)
+            
+        return s_count == t_count
+    
